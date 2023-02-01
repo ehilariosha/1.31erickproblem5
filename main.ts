9f7620048b4d26f3adcec09d441b40c57ba11056
ehilariosha <113388075+ehilariosha@users.noreply.github.com>
2023-02-01 12:18:22 -0800

    saved

--- main.ts
+++ main.ts
@@ -14,6 +14,10 @@ function resetGame () {
 }
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     newXOsprite.setFlag(SpriteFlag.Invisible, false)
+    insertPlayerMove(cursorRow, cursorCol, XorOturn)
+    if (isWinner(XorOturn)) {
+        game.splash(XorOturn, "WINS!")
+    }
     changePlayer()
     XOsprites.push(newXOsprite)
     newXOsprite = textsprite.create(XorOturn)
@@ -51,6 +55,12 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     }
     drawCursor(cursorRow, cursorCol, newXOsprite)
 })
+function insertPlayerMove (row: number, col: number, player2: string) {
+    grid[row][col] = player2
+    for (let value of grid) {
+        console.log(value)
+    }
+}
 function changePlayer () {
     if (XorOturn == "X") {
         XorOturn = "O"
@@ -58,12 +68,49 @@ function changePlayer () {
         XorOturn = "X"
     }
 }
+function isWinner (player2: string) {
+    for (let index = 0; index <= 2; index++) {
+        winningRow = grid[index]
+        if (winningRow[0] == player2) {
+            if (winningRow[0] == winningRow[1] && winningRow[1] == winningRow[2]) {
+                return true
+            }
+        }
+    }
+    for (let index = 0; index <= 2; index++) {
+        winningCol = [grid[0][index], grid[1][index], grid[2][index]]
+        if (winningCol[0] == player2) {
+            if (winningCol[0] == winningCol[1] && winningCol[1] == winningCol[2]) {
+                return true
+            }
+        }
+    }
+    winningDiagonal = [grid[0][0], grid[1][1], grid[2][2]]
+    if (winningDiagonal[0] == player2) {
+        if (winningDiagonal[0] == winningDiagonal[1] && winningDiagonal[1] == winningDiagonal[2]) {
+            return true
+        }
+    }
+    winningDiagonal = [grid[0][2], grid[1][1], grid[2][0]]
+    if (winningDiagonal[0] == player2) {
+        if (winningDiagonal[0] == winningDiagonal[1] && winningDiagonal[1] == winningDiagonal[2]) {
+            return true
+        }
+    }
+    return false
+}
+let winningDiagonal: string[] = []
+let winningCol: string[] = []
+let winningRow: string[] = []
 let newXOsprite: TextSprite = null
 let cursorOnOrOff = 0
 let cursorCol = 0
 let cursorRow = 0
 let XorOturn = ""
 let XOsprites: TextSprite[] = []
+let grid: string[][] = []
+grid = [["", "", ""], ["", "", ""], ["", "", ""]]
+console.log(grid)
 XOsprites = [textsprite.create("")]
 XOsprites.pop()
 drawGrid()
